3dbda77431f2a4e0e4f492d6bb09e7ad5a4d8a3b
arshali8923 <arshali8923@gmail.com>
2026-08-17 18:53:28 +0530

    Add experience_replay.py

---
+++ experience_replay.py
@@ -0,0 +1,19 @@
+from collections import deque
+import random
+
+class ReplayMemory():
+    #create FIFO queue - experiance replay
+
+    def __init__(self , maxlen , seed = None):
+        self.memory = deque([] , maxlen)
+
+    def append(self , new_experience):
+        self.memory.append(new_experience)
+
+    def sample(self , sample_size):
+        return random.sample(self.memory , sample_size)
+
+
+    #function for current buffer size
+    def __len__(self):
+        return len(self.memory)
